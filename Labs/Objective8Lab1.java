910a507836cd5337d9e7d731512500349aa826d2
Omarei <omareigregory@gmail.com>
2021-04-27 14:36:12 -0600

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,12 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+
+    for(int i = 1; i < 11; i ++ ) {
+      System.out.println(i);
+    }
+
+    for(int n = 10; n > 0; n --){
+      System.out.println(n);
+    }
+  }
+}
