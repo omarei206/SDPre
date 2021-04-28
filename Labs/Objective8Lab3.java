ef108a0aa65bdd278a6b15f8b0f5d924b4d98cae
Omarei <omareigregory@gmail.com>
2021-04-27 20:18:03 -0600

    complete objective8lab3

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,16 @@
+public class Objective8Lab3 {
+  public static void main(String[] args) {
+    int counter = 1;
+
+    while(counter <= 20) {
+
+      if(counter % 2 == 0) {
+        System.out.println(counter + " is even");
+      }
+      else {
+        System.out.println(counter + " is odd");
+      }
+      counter += 1;
+    }
+  }
+}
